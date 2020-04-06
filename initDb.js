33277c9535a07e62313ef2f6d96f784510a295fc
Mark Potyomkin <lotuspkt@gmail.com>
2020-04-06 19:03:47 +0500

    add throw

--- initDb.js
+++ initDb.js
@@ -6,6 +6,9 @@ const config = require('./config.json');
 
 exports.init = (name, workdir) => {
     const { latestSaved, salt } = config;
+    if (!salt) {
+        throw new Error('salt is required');
+    }
     const cipherInst = new Cipher(crypto, salt);
     const cacheInst = new Cache(latestSaved || [])
     return new EncryptedDb(name, workdir, cipherInst, cacheInst);
